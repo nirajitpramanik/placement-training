2d19a1a78dc8448bca71ef83432c28ab7173d553
NP15 <76003870+nirajitpramanik@users.noreply.github.com>
2025-04-16 09:48:58 +0530

    Create `combination_sum.py`

---
+++ combination_sum.py
@@ -0,0 +1,20 @@
+def combination_sum(candidates, target):
+    dp = [[] for _ in range(target + 1)]
+    dp[0] = [[]]
+
+    for num in candidates:
+        for i in range(num, target + 1):
+            for comb in dp[i - num]:
+                dp[i].append(comb + [num])
+
+    return dp[target]
+
+if __name__ == "__main__":
+    n = int(input())
+    candidates = list(map(int, input().split()))
+    target = int(input())
+    
+    combinations = combination_sum(candidates, target)
+    
+    for combo in combinations:
+        print(" ".join(map(str, combo)))
